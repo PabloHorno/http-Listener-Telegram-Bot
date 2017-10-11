4ed94746080df78c35669a0affd8c58fe7021a35
Pablo Horno <pablojhorno@gmail.com>
2017-10-11 10:39:01 +0200

    Add project files.

---
+++ http-Listener/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace http_Listener
+{
+    class Program
+    {
+        static string token = "474736832:AAE2d_b7fJBoy_-YCfyhwcxHpguw32_1ows";
+        private static readonly HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            var values = new Dictionary<string,string>
+
+            {
+                "thing1"
+            }
+        }
+    }
+}
